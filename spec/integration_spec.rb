16c82e4bc2582cd44accabb2cd6bb9baed9ee683
Josh Cheek <josh.cheek@gmail.com>
2015-06-19 09:15:15 -0600

    An integration test

---
+++ spec/integration_spec.rb
@@ -0,0 +1,82 @@
+require 'spec_helper'
+
+RSpec.describe 'integration tests' do
+  include IntegrationTestHelpers
+
+  let(:unencrypted_filename) { 'unencrypted' }
+  let(:encrypted_filename)   { 'encrypted' }
+  let(:decrypted_filename)   { 'decrypted' }
+  let(:cracked_filename)     { 'cracked' }
+
+  let(:message)              { 'this is a message..end..' }
+  let(:encrypted_message)    { 'fmotmfzwptg' }
+  let(:key)                  { '26340' }
+  let(:date)                 { '190615' }
+
+  it 'encrypts / decrypts / cracks a message' do
+    cd_proving_grounds do
+      File.write unencrypted_filename, message
+
+      # encrypt
+      result = run 'encrypt', '--in',   unencrypted_filename,
+                              '--out',  encrypted_filename,
+                              '--date', date,
+                              '--key',  key
+      expect(result).to be_success
+      expect(result.stderr).to be_empty
+      expect(result.stdout).to eq "Created '#{encrypted_filename}' with the key #{key} and date #{date}\n"
+      expect(File.read encrypted_filename).to eq encrypted_message
+
+      # decrypt
+      result = run 'decrypt', '--in',   unencrypted_filename,
+                              '--out',  decrypted_filename,
+                              '--date', date,
+                              '--key',  key
+      expect(result).to be_success
+      expect(result.stderr).to be_empty
+      expect(result.stdout).to eq "Created '#{decrypted_filename}' with the key #{key} and date #{date}\n"
+
+      # crack
+      result = run 'crack', '--in', encrypted_filename, '--out', cracked_filename
+      expect(result).to be_success
+      expect(result.stderr).to be_empty
+      expect(result.stdout).to eq "Created '#{cracked_filename}' with the key #{key} and date #{date}\n"
+
+      # files look right, and did not change
+      expect(File.read unencrypted_filename).to eq message
+      expect(File.read encrypted_filename).to_not eq message
+      expect(File.read encrypted_filename).to eq encrypted_message
+      expect(File.read decrypted_filename).to eq message
+      expect(File.read cracked_filename).to eq message
+    end
+  end
+
+  it '(sanity) incorrectly decrypts messages when its date / key are wrong' do
+    # encrypt
+    result = run 'encrypt', '--in',   unencrypted_filename,
+                            '--out',  encrypted_filename,
+                            '--date', '111111',
+                            '--key',  '1111'
+    expect(result).to be_success
+
+    # decrypt bad date
+    result = run 'decrypt', '--in',   unencrypted_filename,
+                            '--out',  decrypted_filename,
+                            '--date', '222222',
+                            '--key',  '1111'
+    expect(result).to be_success
+    decrypted_with_bad_date = File.read decrypted_filename
+    expect(decrypted_with_bad_date).to_not eq message
+
+    # decrypt bad key
+    result = run 'decrypt', '--in',   unencrypted_filename,
+                            '--out',  decrypted_filename,
+                            '--date', '111111',
+                            '--key',  '2222'
+    expect(result).to be_success
+    decrypted_with_bad_key = File.read decrypted_filename
+    expect(decrypted_with_bad_key).to_not eq message
+
+    expect(decrypted_with_bad_key).to_not eq decrypted_with_bad_date
+  end
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,35 @@
+require 'open3'
+
+project_root = File.expand_path '..', __dir__
+bin_path     = File.expand_path project_root, 'bin'
+ENV["PATH"]  = "#{bin_path}:#{ENV["PATH"]}"
+
+module IntegrationTestHelpers
+  def proving_grounds_dir
+    File.expand_path 'proving_grounds', __dir__
+  end
+
+  def delete_files_in(dir)
+    glob = File.join(dir, '*')
+    Dir[glob].each &File.method(:delete)
+    expect(Dir[glob]).to be_empty
+  end
+
+  def cd_proving_grounds(&block)
+    Dir.mkdir proving_grounds_dir unless Dir.exist? proving_grounds_dir
+    Dir.chdir proving_grounds_dir do
+      delete_files_in '.'
+      block.call
+    end
+  end
+
+  Result = Struct.new :stdout, :stderr, :status do
+    def success?
+      status.success?
+    end
+  end
+
+  def run(*args)
+    Result.new *Open3.capture3(*args)
+  end
+end
